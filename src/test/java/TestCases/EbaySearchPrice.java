b8b13b9cf5439aa18afc296a08c1826b8fae385d
dulipub <dulipub@gmail.com>
2025-01-14 21:52:48 +0530

    Added screenshots and reports

--- src/test/java/TestCases/EbaySearchPrice.java
+++ src/test/java/TestCases/EbaySearchPrice.java
@@ -2,7 +2,6 @@ package TestCases;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import Base.BaseTest;
